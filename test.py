fa0808af35ff26b06fff244d2c09adba6a9a3497
doppjk <doppjk@gmail.com>
2020-08-12 01:34:52 +0800

    Update codes for getting price of today

--- test.py
+++ test.py
@@ -2,7 +2,10 @@
 ''' Unittest '''
 
 import time
+import random
 from io import StringIO
+from collections import namedtuple
+
 import requests
 import pandas as pd
 import pickle
@@ -18,13 +21,51 @@ def Update_codes():
     twstock.__update_codes()
 
 def DownloadTWSE_day():
+    df = pd.read_csv('twstock_list.csv')
+    
+    stock_list = df.code
+    stock_market = df.market
+    count = 0
 
-    stock = twstock.Stock(list)
+    year = time.localtime(time.time()).tm_year
+    month = time.localtime(time.time()).tm_mon
+    print(type(stock_list[0]))
+    #for i in range(len(stock_list)):
+    for i in range(3):
+        if stock_market[i] == '上市':    
+            
+            print(stock_list[i])
+            stock = twstock.Stock(str(stock_list[i]))
+            stock.fetch_today()
+            df = pd.DataFrame(stock.data)
+            #df.columns = str(stock_list[i])
+            df.T.to_csv('today.csv', mode='a', header=False, index_label=str(stock_list[i]))   
+            time.sleep(randon.randint(5,8))
     return 1
 
 
 def DownloadOTC_day():
+    return 1
+
+def test_func():
+    stock = twstock.Stock('6215')
+    year = time.localtime(time.time()).tm_year
+    month = time.localtime(time.time()).tm_mon
+    stock.fetch_today()
     
+    
+    NEW_DATATUPLE = namedtuple('Data', ['codes', 'date', 'capacity', 'turnover', 'open',
+                                'high', 'low', 'close', 'change', 'transaction'])
+    new_data = NEW_DATATUPLE(stock.sid, stock.data[0], stock.data[1], stock.data[2], stock.data[3], stock.data[4], stock.data[5], stock.data[6], stock.data[7], stock.data[8])
+    df = pd.DataFrame(new_data)
+    
+    print(df)
+    df.T.to_csv('2330.csv', mode='a', header=False)
+    #print(type(stock.data))
+    #Read file and concat missing data
+    #Check missing days
+    #Insert missing days' data to csv
+    #trasfer_to_csv.to_csv('2330.csv', mode='a')  #To check time stamp
     return 1
 
 if __name__ == '__main__':
@@ -40,24 +81,29 @@ if __name__ == '__main__':
     #To do...
     #CodesRefine() #從TWSE&TPEX篩道只剩'股票'
     #DownloadTWSE_day()
+    test_func()
+    
     #DownloadOTC_day()
     
-    stock = twstock.Stock('2330')
+    
     #stock.fetch_from(2010, 1)#證交所最舊的資料
+    #stock.fetch(year, month) #Cost too much time
     
-    year = time.localtime(time.time()).tm_year
-    month = time.localtime(time.time()).tm_mon
-    stock.fetch(year, month) #Cost too much time
-    #stock.fetch_31()
+#    stock.fetch_31()
+#    trasfer_to_csv = pd.DataFrame(stock.data)
+#    trasfer_to_csv.to_csv('2330.csv', mode='w')
+
+#Good
     
-    #trasfer_to_csv = pd.DataFrame(stock.data)
+    #df = df.DataFrame.append()
+    
+    #cs = pd.read_csv('%d.csv',%sotck_num)
+    #cs = cs.DataFrame.append(stock.data)
+    #cs.insert(stock.data[0], stock.data[1], stock.data[2], stock.data[3], stock.data[4], stock.data[5], stock.data[6], stock.data[7], stock.data[8])
+    #cs.to_csv('2330.csv', mode='w')
     
 
-    #Read file and concat missing data
-    #trasfer_to_csv.to_csv('2330.csv', mode='w')  #To check time stamp
     
-    #pd.read_csv('2330.csv')
-    #f.write(str(trasfer_to_csv))
     tEnd = time.time()#計時開始
     print('Cost time: %f' %(tEnd-tStart))
 
